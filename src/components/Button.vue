914aa3019558b5358bc050535601c1445ad4d5dd
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-04 08:25:01 +0900

    ボタンのpropertyを追加

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -12,6 +12,38 @@
 
 <script>
 export default {
+  props: {
+    label: {
+      type: String,
+      require: true
+    },
+    primary: {
+      type :Boolean,
+      default: false
+    },
+    secondary: {
+      type:Boolean,
+      default: false
+    },
+    size: {
+      type:String,
+      default: 'medium',
+      validator: function(value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== 1
+      }
+    },
+    backgroundColor: {
+      type: String,
+      required: false
+    }
+  },
+  computed: {
+    style() {
+      return {
+        backgroundColor: this.backgroundColor
+      }
+    }
+  }
 
 }
 </script>
