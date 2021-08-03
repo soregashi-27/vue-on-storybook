3b03622fc05b81edae5c8fe15ad3c057fb786313
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-04 08:16:45 +0900

    ボタンcomponentを追加

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button
+      type="button"
+      :class="['button', {primary, secondary}, size]"
+      @click="$emit('onClick')"
+      :style="style"
+      v-text="label"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
