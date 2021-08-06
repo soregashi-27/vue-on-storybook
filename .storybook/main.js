365918e85d0840b703bb37177754d0e7f58f3508
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-06 21:32:42 +0900

    Button storiesを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ module.exports = {
   webpackFinal: config => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-oader'],
+      use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../')
     });
     return config;
